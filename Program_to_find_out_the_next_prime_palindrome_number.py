61035860efed8f6c0f83a677426a14a2ed1150d5
mounika4C3 <84396486+mounika4C3@users.noreply.github.com>
2022-10-07 16:17:45 +0530

    def prime(n):     if n==0 or n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True def pl(n):     if str(n)==str(n)[::-1]:         return True     else:         return False n=int(input()) n+=1 while True:     if prime(n) and pl(n):         print(n)         break     n+=1

---
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -0,0 +1,20 @@
+def prime(n):
+    if n==0 or n==1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+def pl(n):
+    if str(n)==str(n)[::-1]:
+        return True
+    else:
+        return False
+n=int(input())
+n+=1
+while True:
+    if prime(n) and pl(n):
+        print(n)
+        break
+    n+=1
